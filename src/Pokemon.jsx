418c626d03cc0d61f2de8301475318fe8b49f553
Bowen Suico <160026137+bowssing@users.noreply.github.com>
2024-03-16 20:23:12 +0800

    Add files via upload

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,4 +1,22 @@
-
+const typeColors = {
+  'Grass': '#78c850',
+  'Fire': '#f08030',
+  'Water': '#6890f0',
+  'Bug': '#a8b820',
+  'Normal': '#a8a878',
+  'Poison': '#a040a0',
+  'Flying': '#a890f0',
+  'Electric': '#f8d030',
+  'Ground': '#e0c068',
+  'Fairy': '#ee99ac',
+  'Fighting': '#c03028',
+  'Psychic': '#f85888',
+  'Rock': '#b8a038',
+  'Steel': '#b8b8d0',
+  'Ice': '#98d8d8',
+  'Ghost': '#705898',
+  'Dragon': '#7038f8',
+};
 function Pokemon ({ pokemon, language }) {
     
     const { id, name, type, base, image } = pokemon;
@@ -22,8 +40,11 @@ function Pokemon ({ pokemon, language }) {
         <div className="pokemon">
             <img className="image" src={image}/>
             <h1 className="title">[#{id}] {getNameByLanguage()}</h1>
-            <h2 className="type">{type.join(', ')}</h2>
-
+            <div className="type">
+            {pokemon.type.map((type, index) => (
+          <span key={index} className="type" style={{ backgroundColor: typeColors[type] }}>{type}</span>
+        ))}
+            </div>
             <div className="base">
             <ul>
                 <li>HP: {base.HP}</li>
